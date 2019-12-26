40cccc35a4fc2dea12bcab977c0117d9f0a82b19
Maika Kailani <queenmab051@gmail.com>
2019-12-26 16:41:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,23 +1,10 @@
 def prime?(number)
-<<<<<<< HEAD
-  if number <= 1
-    return false
-  elsif number <= 3
-    return true
-  else (2..number/2).none? do |x|
-    number % x == 0
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_to_test| 
     end
+  else
+    false
   end
-=======
-  if number <= 1 || number == 0 || number == 1
-    return false
-  elsif
-    (2..number - 1).each do |i|
-      if numbere % i == 0
-        return false
-      end
-    end
-  end
-  true
->>>>>>> 2c321e8267611549a143f7e0fb6ca85e9338cbff
 end
