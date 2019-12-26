1bd7153be2ee51db62414e32d462bc16489b9355
Maika Kailani <queenmab051@gmail.com>
2019-12-26 16:39:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,5 @@
 def prime?(number)
+<<<<<<< HEAD
   if number <= 1
     return false
   elsif number <= 3
@@ -7,4 +8,16 @@ def prime?(number)
     number % x == 0
     end
   end
+=======
+  if number <= 1 || number == 0 || number == 1
+    return false
+  elsif
+    (2..number - 1).each do |i|
+      if numbere % i == 0
+        return false
+      end
+    end
+  end
+  true
+>>>>>>> 2c321e8267611549a143f7e0fb6ca85e9338cbff
 end
